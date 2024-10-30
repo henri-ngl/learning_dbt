47a797eb5feb30a5b108efe61238af001a835018
Henri Nguemaleu <henri.nguemaleu@gmail.com>
2024-10-30 06:39:08 +0100

    Henri has generated an Uninstall powershell script for browser(s) Chrome for client 529eb260-c82e-11ea-a5b5-6bd9fe6e2a87

--- powershell/client_scripts/529eb260-c82e-11ea-a5b5-6bd9fe6e2a87/Chrome_Uninstall.ps1
+++ powershell/client_scripts/529eb260-c82e-11ea-a5b5-6bd9fe6e2a87/Chrome_Uninstall.ps1
@@ -14,6 +14,7 @@
 #region Variables
 
 $ErrorActionPreference = 'Stop'
+$Remove3rdPartyFirefoxRegistry = $false # Set to $false if you want to keep the 3rdparty Extensions registry entries.
 $ForceUpdateUserPolicy = $false # Set to $true to force update user policy values for all users
 
 
@@ -26,6 +27,17 @@ $BeamyChromeExtension = @{
 $BeamyChromeExtension.PolicyPath = Join-Path -Path $BeamyChromeExtension.PolicyPathBase -ChildPath "$($BeamyChromeExtension.ExtensionId)/policy"
 
 
+$BeamyFirefoxExtension = @{
+    ExtensionId = 'support.firefox@beamy.io'
+    ExtensionUpdateUrl = 'https://storage.googleapis.com/bmy-data-prod-wbe-public-files/prod/firefox/beamy-latest.xpi'
+    InstallPath = "HKLM:\SOFTWARE\Policies\Mozilla\Firefox\Extensions\Install"
+    LockedPath = "HKLM:\SOFTWARE\Policies\Mozilla\Firefox\Extensions\Locked"
+    PolicyPathBase = "HKLM:\SOFTWARE\Policies\Mozilla\Firefox\3rdparty\Extensions\"
+    ThirdPartyPath = "HKLM:\SOFTWARE\Policies\Mozilla\Firefox\3rdparty" # Used to remove legacy Beamy entries
+    PoliciesJsonPath = $null
+    FirefoxInstallationPath = $null # Replace with the path to the Firefox installation folder if known, otherwise let the script find it.
+}
+$BeamyFirefoxExtension.PolicyPath = Join-Path -Path $BeamyFirefoxExtension.PolicyPathBase -ChildPath $BeamyFirefoxExtension.ExtensionId
 
 $BeamyGlobalVariables = @{
     ClientID = '529eb260-c82e-11ea-a5b5-6bd9fe6e2a87'
@@ -77,6 +89,46 @@ function Uninstall-BeamyChrome {
 }
 
 
+function Uninstall-BeamyFirefox {
+    Write-Host "Uninstalling Beamy's Firefox extension..."
+
+    # Remove from Install path
+    if (Test-Path $BeamyFirefoxExtension.InstallPath) {
+        $properties = Get-ItemProperty -Path $BeamyFirefoxExtension.InstallPath
+        $propertiesToRemove = $properties.PSObject.Properties | Where-Object { $_.Value -like "*$($BeamyFirefoxExtension.ExtensionUpdateUrl)*" }
+        foreach ($prop in $propertiesToRemove) {
+            Remove-RegistryValue -Path $BeamyFirefoxExtension.InstallPath -Name $prop.Name
+        }
+    }
+
+    # Remove from Locked path
+    if (Test-Path $BeamyFirefoxExtension.LockedPath) {
+        $properties = Get-ItemProperty -Path $BeamyFirefoxExtension.LockedPath
+        $propertiesToRemove = $properties.PSObject.Properties | Where-Object { $_.Value -eq $BeamyFirefoxExtension.ExtensionId }
+        foreach ($prop in $propertiesToRemove) {
+            Remove-RegistryValue -Path $BeamyFirefoxExtension.LockedPath -Name $prop.Name
+        }
+    }
+
+    # Remove extension policy
+    Remove-RegistryKey -Path $BeamyFirefoxExtension.PolicyPath
+
+    # Update policies.json
+    $FirefoxPath = Find-FirefoxInstallationPath
+    if ($FirefoxPath) {
+        $policiesJsonPath = Join-Path $FirefoxPath "distribution\policies.json"
+        if (Test-Path $policiesJsonPath) {
+            $policiesJson = Get-Content $policiesJsonPath -Raw | ConvertFrom-Json
+            if ($policiesJson.policies.ExtensionSettings -and $policiesJson.policies.ExtensionSettings.$($BeamyFirefoxExtension.ExtensionId)) {
+                $policiesJson.policies.ExtensionSettings.$($BeamyFirefoxExtension.ExtensionId).installation_mode = "blocked"
+                $policiesJson | ConvertTo-Json -Depth 10 | Set-Content $policiesJsonPath -Force
+                Write-Host "Updated Beamy extension policy to 'blocked' in policies.json"
+            }
+        }
+    }
+
+    Write-Host "Beamy's Firefox extension has been uninstalled."
+}
 function Remove-UserPolicies {
     Write-Host "Removing Beamy's web browser extension policies for all users..."
 
@@ -88,6 +140,7 @@ function Remove-UserPolicies {
         $Sid = $UserProfile.SID
         if (Test-Path -Path "${HKUsersRegistryName}:\${Sid}") {
             Remove-RegistryKey -Path $($BeamyChromeExtension.PolicyPath -replace "HKLM:\\", "${HKUsersRegistryName}:\${Sid}\")
+            Remove-RegistryKey -Path $($BeamyFirefoxExtension.PolicyPath -replace "HKLM:\\", "${HKUsersRegistryName}:\${Sid}\")
         }
     }
     Remove-PSDrive -Name $HKUsersRegistryName
@@ -103,6 +156,7 @@ switch ('Uninstall') {
 
     "Uninstall" {
         Uninstall-BeamyChrome
+        Uninstall-BeamyFirefox
         Remove-UserPolicies
         Write-Host "Beamy's web browser extension has been uninstalled from all supported browsers."
     }
@@ -112,3 +166,4 @@ switch ('Uninstall') {
 }
 
 #endregion
+coucou
